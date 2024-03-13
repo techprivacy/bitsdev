2b23ebaff660f3f2c2ed534fdb8b1fecca427238
Yogesh <techprivacy@gmail.com>
2024-03-13 23:43:21 +0530

    updated

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload. Bits
+          Edit <code>src/App.js</code> and save to reload. BITS Pilani
         </p>
         <a
           className="App-link"
